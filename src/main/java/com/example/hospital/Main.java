da5a55538adae7d8f87b67cd1f4612d4650fdc93
Diaschk <yovhymyschdiana@gmail.com>
2025-12-12 13:19:13 +0200

    Update Main.java

--- src/main/java/com/example/hospital/Main.java
+++ src/main/java/com/example/hospital/Main.java
@@ -1,5 +1,10 @@
 package com.example.hospital;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-public class Main {
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }

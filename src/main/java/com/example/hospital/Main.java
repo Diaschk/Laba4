6938f90e9a6d2f7a4d02c33d4ed89f0d2d2c5171
Diaschk <yovhymyschdiana@gmail.com>
2025-12-12 15:32:23 +0200

    Update Main.java

--- src/main/java/com/example/hospital/Main.java
+++ src/main/java/com/example/hospital/Main.java
@@ -1,10 +1,27 @@
 package com.example.hospital;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+}
+
+@RestController
+class HelloController {
+    
+    @GetMapping("/")
+    public String home() {
+        return "Вітаю! Госпітальний додаток запущено на AWS EC2!";
+    }
+    
+    @GetMapping("/test")
+    public String test() {
+        return "Тестова сторінка для лабораторної роботи №5";
     }
 }
